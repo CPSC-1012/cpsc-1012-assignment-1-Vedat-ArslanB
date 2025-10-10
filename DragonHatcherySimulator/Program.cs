8cd3317c0f7f822a324d0831b9a96362ab43e6a9
Vedat Arslan <varslan1@nait.ca>
2025-10-10 13:25:38 -0600

    Made some calculations and changes debuged and tested

--- DragonHatcherySimulator/Program.cs
+++ DragonHatcherySimulator/Program.cs
@@ -11,11 +11,20 @@ namespace DragonHatcherySimulator
 
             // Gets the user input for the num of dragons
             Console.Write("Enter the inintial number of dragons: ");
-            int inintialDragons = int.Parse(Console.ReadLine());
+            int initialDragons = int.Parse(Console.ReadLine());
 
             Console.Write("Enter the daily growth rate(Percentage): ");
             double growthRatePerc = double.Parse(Console.ReadLine());
             double growthRate = growthRatePerc / 100.0;
+
+            Console.Write("Enter the number of weeks: ");
+            int weeks = int.Parse(Console.ReadLine());
+            int days = weeks * 7;
+
+            double total = initialDragons * Math.Pow(1 + growthRate, days);
+            int roundTotal = (int)Math.Round(total);
+
+            Console.WriteLine($"\nAfter {days} days, there will be approximately {roundTotal} dragons in the hatchery.");
 
         }
     }
