425e4477b0fb24186feee755f3d17949912c1966
Vedat Arslan <varslan1@nait.ca>
2025-10-10 13:14:17 -0600

    Calculations

--- DragonHatcherySimulator/Program.cs
+++ DragonHatcherySimulator/Program.cs
@@ -10,10 +10,12 @@ namespace DragonHatcherySimulator
             Console.WriteLine("Welcome to Dragon Hatchery Simulator!");
 
             // Gets the user input for the num of dragons
-            Console.WriteLine("Enter the inintial number of dragons: ");
-            int inintialDragons = int.Parse(Console.ReadLine()!);
-            
+            Console.Write("Enter the inintial number of dragons: ");
+            int inintialDragons = int.Parse(Console.ReadLine());
 
+            Console.Write("Enter the daily growth rate(Percentage): ");
+            double growthRatePerc = double.Parse(Console.ReadLine());
+            double growthRate = growthRatePerc / 100.0;
 
         }
     }
