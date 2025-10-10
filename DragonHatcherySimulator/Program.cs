5735a7a493c2264fb1f51fd0cd32854ac9f5019f
Vedat Arslan <varslan1@nait.ca>
2025-10-10 13:09:06 -0600

    Made some changes and started the project

--- DragonHatcherySimulator/Program.cs
+++ DragonHatcherySimulator/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace DragonHatcherySimulator
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+            Console.WriteLine("Welcome to Dragon Hatchery Simulator!");
+
+            // Gets the user input for the num of dragons
+            Console.WriteLine("Enter the inintial number of dragons: ");
+            int inintialDragons = int.Parse(Console.ReadLine()!);
+            
+
+
+        }
+    }
+}
